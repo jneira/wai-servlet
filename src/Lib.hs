b42a2e814228db482b95f4a7a3eecefe5d06beb5
jneira <atreyu.bbb@gmail.com>
2017-02-27 06:45:12 +0100

    java interop test

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,3 +1,19 @@
+{-# LANGUAGE MagicHash #-}
+
+module Lib where
+
+import Java
+
+data {-# CLASS "javax.servlet.Servlet" #-} Servlet = Servlet (Object# Servlet)
+data {-# CLASS "javax.servlet.ServletRequest" #-} ServletRequest =
+  ServletRequest (Object# ServletRequest)
+data {-# CLASS "javax.servlet.ServletResponse" #-} ServletResponse =
+  ServletResponse (Object# ServletResponse)
+
+
+foreign import java unsafe "@interface service" service
+  :: Servlet -> ServletRequest -> ServletResponse -> IO ()
+
 module Lib
     ( someFunc
     ) where
