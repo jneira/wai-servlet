28068a305b37ed4999e1603e554e254e64a8ca48
jneira <atreyu.bbb@gmail.com>
2017-03-02 06:21:44 +0100

    create a java class WAIServlet

---
+++ java/WAIServlet.java
@@ -0,0 +1,13 @@
+import javax.servlet.GenericServlet;
+import javax.servlet.HttpRequest;
+import javax.servlet.HttpResponse;
+
+public class WAIServlet extends GenericServlet {
+
+    
+    
+    public void service (HttpRequest req, HttpResponse resp) {
+        
+    }
+    
+}
--- src/Lib.hs
+++ src/Lib.hs
@@ -9,15 +9,16 @@ data {-# CLASS "javax.servlet.ServletRequest" #-} ServletRequest =
 data {-# CLASS "javax.servlet.ServletResponse" #-} ServletResponse =
   ServletResponse (Object# ServletResponse)
 
+foreign import java unsafe "@wrapper @abstract service "
+  service :: (ServletRequest -> ServletResponse -> Java GenericServlet()) -> GenericServlet 
 
-data {-# CLASS "network.wai.servlet.WAIServlet" #-} WAIServlet =
-  WAIServlet (Object# WAIServlet) deriving Class
+data {-# CLASS "network.wai.servlet.WAIServlet extends javax.servlet.GenericServlet" #-}
+  WAIServlet = WAIServlet (Object# WAIServlet) deriving Class
 
 type instance Inherits WAIServlet = '[GenericServlet]
 
-service :: ServletResponse -> Java WAIServlet ()
-service = undefined
+service1 :: ServletRequest -> ServletResponse -> Java WAIServlet ()
+service1 = undefined
 
-foreign export java service :: ServletRequest -> ServletResponse ->
-                               Java WAIServlet ()
-
+foreign export java service1 :: ServletRequest -> ServletResponse ->
+                                Java WAIServlet ()
