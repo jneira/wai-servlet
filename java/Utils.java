1f4c8ea6f34120552341dd1decd1892f85c5856d
jneira <atreyu.bbb@gmail.com>
2017-03-14 09:46:06 +0100

    correct buffer reference

--- java/Utils.java
+++ java/Utils.java
@@ -5,8 +5,7 @@ import eta.runtime.io.MemoryManager;
 
 public class Utils {
     public static byte[] toByteArray(ByteBuffer ptr, int offset, int length) {
-        int memAddr=MemoryManager.bufGetInt(ptr,0);
-        ByteBuffer buf=MemoryManager.getBuffer(memAddr);
+        ByteBuffer buf=ptr.duplicate();
         buf=MemoryManager.bufSetOffset(buf,offset);
         buf=MemoryManager.bufSetLimit(buf,length);
         byte[] b = new byte[buf.remaining()];
