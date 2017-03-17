aebf3b31ade7f2a00a700c5e9be1c50cd76190a8
jneira <atreyu.bbb@gmail.com>
2017-03-18 00:32:36 +0100

    formatting

--- src/Network/Wai/Servlet/Request.hs
+++ src/Network/Wai/Servlet/Request.hs
@@ -3,11 +3,13 @@ module Network.Wai.Servlet.Request where
 import qualified Network.Wai as Wai
 import Java
 
-data {-# CLASS "javax.servlet.ServletRequest" #-} ServletRequest =
-  ServletRequest (Object# ServletRequest) deriving Class
+data {-# CLASS "javax.servlet.ServletRequest" #-}
+  ServletRequest = ServletRequest (Object# ServletRequest)
+  deriving Class
 
-data {-# CLASS "javax.servlet.http.HttpServletRequest" #-} HttpServletRequest =
-  HttpServletRequest (Object# HttpServletRequest) deriving Class
+data {-# CLASS "javax.servlet.http.HttpServletRequest" #-}
+  HttpServletRequest = HttpServletRequest (Object# HttpServletRequest)
+  deriving Class
 
 type instance Inherits HttpServletRequest = '[ServletRequest]
 
