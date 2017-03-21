3f7ba5ed7ae140bc5fa0de777c786c25fe901a64
jneira <atreyu.bbb@gmail.com>
2017-03-21 06:47:44 +0100

    continue buildinf wai request from servlet one

--- src/Network/Wai/Servlet/Request.hs
+++ src/Network/Wai/Servlet/Request.hs
@@ -1,11 +1,13 @@
 {-# LANGUAGE MagicHash,TypeFamilies,DataKinds,FlexibleContexts,
              OverloadedStrings, TypeOperators #-}
 module Network.Wai.Servlet.Request where
-import Network.Wai.Internal
+import qualified Network.Wai.Internal as W
 import qualified Network.HTTP.Types as H
 import Network.Socket (SockAddr (SockAddrInet))
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Char8 as BSChar (pack)
+import qualified Data.ByteString.UTF8 as BSUTF8 (toString)
+import Data.List (intercalate)
 import Java
 
 data {-# CLASS "javax.servlet.ServletRequest" #-}
@@ -18,42 +20,62 @@ data {-# CLASS "javax.servlet.http.HttpServletRequest" #-}
 
 type instance Inherits HttpServletRequest = '[ServletRequest]
 
-foreign import java unsafe "@interface getMethod" getMethod ::
-  (a <: HttpServletRequest) => Java a String
+foreign import java unsafe "@interface getCharacterEncoding" getCharacterEncoding ::
+  (a <: ServletRequest) => Java a String
 foreign import java unsafe "@interface getVersion" getProtocol ::
   (a <: ServletRequest) => Java a String
+foreign import java unsafe "@interface getMethod" getMethod ::
+  (a <: HttpServletRequest) => Java a String
+foreign import java unsafe "@interface getPathInfo" getPathInfo ::
+  (a <: HttpServletRequest) => Java a String
+foreign import java unsafe "@interface getHeaderNames" getHeaderNames ::
+  (a <: HttpServletRequest) => Java a (Enumeration String)
+foreign import java unsafe "@interface getHeaders" getHeaders ::
+  (a <: HttpServletRequest) => String -> Java a (Enumeration String)
 
 
-makeWaiRequest :: HttpServletRequest -> Request
-makeWaiRequest req  =  Request
-   { requestMethod = getRequestMethod req 
-   , httpVersion = getHttpVersion req
-   , rawPathInfo = B.empty
-   , rawQueryString = B.empty
-   , requestHeaders = []
-   , isSecure = False
-   , remoteHost = SockAddrInet 0 0
-   , pathInfo = []
-   , queryString = []
-   , requestBody = return B.empty
-   , vault = mempty
-   , requestBodyLength = KnownLength 0
-   , requestHeaderHost = Nothing
-   , requestHeaderRange = Nothing
-   , requestHeaderReferer = Nothing
-   , requestHeaderUserAgent = Nothing
+makeWaiRequest :: HttpServletRequest -> W.Request
+makeWaiRequest req  =  W.Request
+   { W.requestMethod = requestMethod req 
+   , W.httpVersion = httpVersion req
+   , W.rawPathInfo = pathInfo req
+   , W.rawQueryString = B.empty
+   , W.requestHeaders = []
+   , W.isSecure = False
+   , W.remoteHost = SockAddrInet 0 0
+   , W.pathInfo = H.extractPath $ pathInfo req
+   , W.queryString = []
+   , W.requestBody = return B.empty
+   , W.vault = mempty
+   , W.requestBodyLength = W.KnownLength 0
+   , W.requestHeaderHost = Nothing
+   , W.requestHeaderRange = Nothing
+   , W.requestHeaderReferer = Nothing
+   , W.requestHeaderUserAgent = Nothing
   }
 
-getRequestMethod :: (a <: HttpServletRequest) => a -> H.Method
-getRequestMethod req = pureJavaWith req $ do
+requestMethod :: (a <: HttpServletRequest) => a -> H.Method
+requestMethod req = pureJavaWith req $ do
   method <- getMethod
   return $ BSChar.pack method
 
-getHttpVersion ::  (a <: ServletRequest) => a -> H.HttpVersion
-getHttpVersion req = pureJavaWith req $ do 
+httpVersion ::  (a <: ServletRequest) => a -> H.HttpVersion
+httpVersion req = pureJavaWith req $ do 
   httpVer <- getProtocol
   return $ case httpVer of
     "HTTP/0.9" -> H.http09
     "HTTP/1.0" -> H.http10
     "HTTP/1.1" -> H.http11
+  
+pathInfo :: (a <: ServletRequest) => a -> B.ByteString
+pathInfo req = pureJavaWith req $ do
+  path <- getPathInfo
+  return $ BSUTF8.fromString path
+
+requestHeaders :: (a <: ServletRequest) => a -> H.RequestHeaders
+requestHeaders req = pureJavaWith req $ do
+  names <- fromJava getHeaderNames
+  return $  zip names $ map (BSUTF8.fromString . intercalate "," .
+                             fromJava . getHeaders) names
+  
   
