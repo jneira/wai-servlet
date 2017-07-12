7a7ded355b65ebf4eea6a6face879d3666f60515
Rahul Muttineni <rahulmutt@gmail.com>
2017-07-12 18:10:15 -0400

    Update for Eta v0.0.9

---
+++ java/Utils2.java
@@ -0,0 +1,48 @@
+package network.wai.servlet;
+
+import java.nio.ByteBuffer;
+import eta.runtime.io.MemoryManager;
+import java.io.InputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+public class Utils2 {
+
+    public static byte[] toByteArray(long ptr, int offset, int length) {
+        ByteBuffer buf = MemoryManager.getBoundedBuffer(ptr);
+        buf.position(buf.position() + offset);
+        buf.limit(buf.position() + length);
+        byte[] b = new byte[buf.remaining()];
+        buf.get(b);
+        return b;
+    }
+
+    public static ByteBuffer toByteBuffer(InputStream is)
+      throws IOException {
+        byte[] bytes=toByteArray(is);
+        return ByteBuffer.wrap(bytes);
+    }
+
+    // from apache.commons.io.IOUtils
+    private static final int EOF = -1;
+
+    private static final int DEFAULT_BUFFER_SIZE = 1024 * 4;
+
+    public static byte[] toByteArray (InputStream input) throws
+        IOException {
+        byte[] buffer=new byte[DEFAULT_BUFFER_SIZE];
+        try (final ByteArrayOutputStream output =
+             new ByteArrayOutputStream()) {
+            long count = 0;
+            int n;
+            while (EOF != (n = input.read(buffer))) {
+                output.write(buffer, 0, n);
+                count += n;
+            }
+            return output.toByteArray();
+        }
+    }
+    public static int size(ByteBuffer buf) {
+        return buf.remaining();
+    }
+}
